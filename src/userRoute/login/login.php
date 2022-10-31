61ee4048875f895153e150310842062fb51f65e2
hayategari <hayatesato1@gmail.com>
2022-10-31 12:31:40 +0900

    a

--- src/userRoute/login/login.php
+++ src/userRoute/login/login.php
@@ -2,43 +2,56 @@
 
 session_start();
 
-// 値の受け取り
-$email = filter_input(INPUT_POST, 'email');
-$password = filter_input(INPUT_POST, 'password');
+// クラスファイルインポート
+require __DIR__ . '../../../../class/UserLogic.php';
 
-// エラー配列
+// functionファイルインポート
+require __DIR__ . '../../../../function/functions.php';
+
+// クラスのインポート
+$obj = new UserLogic();
+
+// err配列準備
 $err = [];
 
-// ユーザロジックファイルの読み込み
-require '/Applications/MAMP/htdocs/Deliverables3/class/userLogic.php';
+// フォームリクエストを受け取る
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-$result = UserLogic::login($email, $password);
+    $email = filter_input(INPUT_POST, 'email');
+    $password = filter_input(INPUT_POST, 'password');
 
-if (!$result) {
-    $err[] = 'ログインに失敗しました。';
+    // ログインメソッドを実行
+    $result = $obj::login($email, $password);
+
+    // ユーザ存在なし、パスワード不一致の場合エラーを出す
+    if (!$result) {
+        $err[] = '認証に失敗しました。';
+    }
+}else{
+    $err[] = '不正なリクエストです。';
+
+    // 3秒後に入力フォームへリダイレクト
+    header('refresh:3;url=./login_form.php');
 }
+
 
 ?>
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../../../public/img/favicon.ico">
-    <link rel="stylesheet" href="../../../public/css/login/login.css">
-    <title>「Real intentioN」 / ログイン</title>
+    <title>Document</title>
 </head>
 
 <body>
-    <?php include '/Applications/MAMP/htdocs/Deliverables3/public/template/header.html'; ?>
-
     <div class="err">
         <?php if (count($err) > 0) : ?>
             <?php foreach ($err as $e) : ?>
-                <label><?php echo $e; ?></label>
+                <label><?php h($e); ?></label>
                 <div class="backBtn">
                     <a href="./login_form.php">戻る</a>
                 </div>
@@ -46,10 +59,7 @@ if (!$result) {
         <?php endif; ?>
 
         <?php if (count($err) === 0) : ?>
-            <label>ログインに成功しました。</label>
-            <!-- <div class="backBtn">
-                <a href="../intern/view.php">ホームへ</a>
-            </div> -->
+            <label>ログインが完了しました。</label>
             <?php header('refresh:3;url=../intern/view.php'); ?>
         <?php endif; ?>
     </div>
--- src/userRoute/login/login_form.php
+++ src/userRoute/login/login_form.php
@@ -1,45 +1,24 @@
 <?php
 
-// // セッションスタート
-// session_start();
-
-// // セッション情報があるか判定をする
-// if (!isset($_SESSION['access_token'])) {
-//     $_SESSION['err'] = '不正なリクエストです';
-//     header('Location: auth_form.php');
-//     exit();
-// }
-
-// $email = $_SESSION['email'];
 
 ?>
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../../../public/img/favicon.ico">
-    <link rel="stylesheet" href="../../../public/css/register/student_infomation.css">
-    <title>「Real intentioN」 / ログイン</title>
+    <title>Document</title>
 </head>
 
 <body>
-
-    <?php include '/Applications/MAMP/htdocs/Deliverables3/public/template/header.html'; ?>
-
-    <div class="authContent">
-        <h2>ログイン</h2>
-        <form action="./login.php" method="post">
-            <p><span>必須</span><label style="margin-right: 10px;">メールアドレス</label><input type="text" name="email" required></p>
-
-            <p><span>必須</span><label style="margin-right: 48px;">パスワード</label><input type="password" name="password" required></p>
-            
-            <button type="submit">認証する</button>
-        </form>
-    </div>
+    <form action="./login.php" method="post">
+        <p><label>メールアドレス</label><input type="text" name="email"></p>
+        <p><label>パスワード</label><input type="password" name="password"></p>
+        <button type="submit">ログイン</button>
+    </form>
 </body>
 
 </html>
