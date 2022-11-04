375e992e81acb8e970b4ebe9667fcdda67246954
hayategari <hayatesato1@gmail.com>
2022-11-05 04:11:57 +0900

    ログイン、インターン体験記完了

--- class/Logic.php
+++ class/Logic.php
@@ -1,20 +1,61 @@
 <?php
 
-// データベースロジックファイルの読み込み
-require '/Applications/MAMP/htdocs/Deliverables3/class/DatabaseLogic.php';
+// DB接続ファイルの読み込み
+require '/Applications/MAMP/htdocs/Deliverables3/function/connect_db.php';
 
 // -----------------------------------------------------------------------------------------------------------------
 
 // ユーザ情報を扱うクラス
 class UserLogic
 {
-}
+    // メールアドレスが存在するか確認する
+    public static function user_exist_check($email)
+    {
+        $db_obj = new DatabaseLogic();
 
-// -----------------------------------------------------------------------------------------------------------------
+        $sql = 'SELECT * FROM user_master WHERE email = ?';
 
-// バリデーション、正規表現を扱うクラス
-class ValidationLogic
-{
+        // SQL実行
+        $result = $db_obj::db_select_arr($sql, $email);
+
+        // データがあればデータが返る　
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
+
+    // ログインする
+    public static function login_execution($email, $password)
+    {
+        // メールアドレスが存在するか判定する
+        $userData = self::user_exist_check($email);
+
+        // データが存在しない(返り値がTrue)であればエラーとする
+        if (!$userData) {
+            return false;
+        }
+
+        // データが存在した場合パスワード認証を行う
+        if ($userData) {
+
+            // DBのパスワードを取得
+            foreach ($userData as $row) {
+                $db_password = $row['password'];
+            }
+
+            if (password_verify($password, $db_password)) {
+
+                //ログイン成功の場合 trueを返す
+                session_regenerate_id(true);
+                $_SESSION['login_user'] = $userData;
+                return $_SESSION['login_user'];
+            } else {
+                return false;
+            }
+        }
+    }
 }
 
 // -----------------------------------------------------------------------------------------------------------------
@@ -22,6 +63,34 @@ class ValidationLogic
 // 投稿を扱うクラス
 class PostLogic
 {
+    // ログインしているかどうか判定する
+    public static function login_check()
+    {
+        // ユーザ情報があればログインしているとみなす return true
+        if (isset($_SESSION['login_user'])) {
+            $result = $_SESSION['login_user'];
+            return $result;
+        }
+
+        // セッション情報がない場合はログインしていないとみなす return false;
+        return false;
+    }
+
+    // テーブル全てのレコードを取得する
+    public static function post_acquisition($sql)
+    {
+        $db_obj = new DatabaseLogic();
+
+        //  SELECTメソッド実行
+        $result = $db_obj::db_select($sql);
+
+        // データがあった場合データを返す
+        if ($result) {
+            return $result;
+        }
+
+        return false;
+    }
 }
 
 // -----------------------------------------------------------------------------------------------------------------
@@ -29,6 +98,99 @@ class PostLogic
 // データベースを扱うクラス
 class DatabaseLogic
 {
+    // SELECTメソッド（パラメータなし）
+    public static function db_select($sql)
+    {
+        try {
+            // sql実行
+            $stmt = connect()->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+
+            // データを返す
+            return $result;
+        } catch (\Exception $e) {
+            echo $e;
+            error_log($e, 3, '../error.log');
+
+            // エラーの場合Falseを返す
+            return false;
+        }
+    }
+
+    // SELECTメソッド（パラメータあり）
+    public static function db_select_arr($sql, $arr)
+    {
+        try {
+            // sql実行
+            $stmt = connect()->prepare($sql);
+            $stmt->execute(array($arr));
+            $result = $stmt->fetchAll();
+
+            // データを返す
+            return $result;
+        } catch (\Exception $e) {
+            echo $e;
+            error_log($e, 3, '../error.log');
+
+            // エラーの場合Falseを返す
+            return false;
+        }
+    }
+
+    // INSERTメソッド
+    public static function db_insert($sql, $arr)
+    {
+        try {
+            $stmt = connect()->prepare($sql);
+            $stmt->execute($arr);
+
+            // 実行成功の場合
+            return true;
+        } catch (\Exception $e) {
+            echo $e;
+            error_log($e, 3, '../error.log');
+
+            // 実行失敗
+            return false;
+        }
+    }
+
+    // UPDATEメソッド
+    public static function db_update($sql, $arr)
+    {
+        try {
+            $stmt = connect()->prepare($sql);
+            $stmt->execute($arr);
+
+            // 更新成功
+            return true;
+        } catch (\Exception $e) {
+            echo $e;
+            error_log($e, 3, '../error.log');
+
+            // 更新失敗
+            return false;
+        }
+    }
+
+    // DELETEメソッド
+    public static function db_delete($sql, $arr)
+    {
+        try {
+            $stmt = connect()->prepare($sql);
+            $stmt->execute($arr);
+
+            // 削除完了
+            return true;
+        } catch (\Exception $e) {
+            echo $e;
+            error_log($e, 3, '../error.log');
+
+            // 削除失敗
+            return false;
+        }
+    }
 }
 
 // -----------------------------------------------------------------------------------------------------------------
---
+++ src/UserView/login/login.php
@@ -0,0 +1,74 @@
+<?php
+
+session_start();
+
+// クラスファイルインポート
+require __DIR__ . '../../../../class/Logic.php';
+
+// functionファイルインポート
+require __DIR__ . '../../../../function/functions.php';
+
+// クラスのインポート
+$user_obj = new UserLogic();
+
+// errメッセージが入る配列準備
+$err = [];
+
+// フォームリクエストを受け取る
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // バリーデーションチェック
+    if (!$email = filter_input(INPUT_POST, 'email')) {
+        $err[] =  'メールアドレスを入力してください。';
+    }
+
+    if (!$password = filter_input(INPUT_POST, 'password')) {
+        $err[] =  'パスワードを入力してください。';
+    }
+
+    // ログインメソッドを実行
+    $login = $user_obj::login_execution($email, $password);
+
+    // ユーザ存在なし、パスワード不一致の場合エラーを出す
+    if (!$login) {
+        $err[] = 'ログインに失敗しました。';
+    }
+} else {
+    $err[] = '不正なリクエストです。';
+
+    // 3秒後に入力フォームへリダイレクト
+    header('refresh:3;url=./login_form.php');
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="err">
+        <?php if (count($err) > 0) : ?>
+            <?php foreach ($err as $e) : ?>
+                <label><?php h($e); ?></label>
+            <?php endforeach; ?>
+            <div class="backBtn">
+                <a href="./login_form.php">戻る</a>
+            </div>
+        <?php endif; ?>
+
+        <?php if (count($err) === 0) : ?>
+            <label>ログインが完了しました。</label>
+            <?php header('refresh:3;url=../intern_experience/view.php'); ?>
+        <?php endif; ?>
+    </div>
+</body>
+
+</html>
