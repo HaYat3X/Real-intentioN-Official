19978fa1b17f2b319c19aecf44696f3aee886cd4
hayategari <hayatesato1@gmail.com>
2022-11-16 11:37:45 +0900

    削除機能のデザイン修正

--- src/UserView/Intern_experience/delete/delete_check.php
+++ src/UserView/Intern_experience/delete/delete_check.php
@@ -15,6 +15,9 @@ $student_inst = new StudentLogics();
 // ログインチェック
 $userId = $student_inst->get_student_id();
 
+// 学生の名前
+$userName = $student_inst->get_student_name();
+
 // ログインチェックの返り値がfalseの場合ログインページにリダイレクト　（不正なリクエストとみなす）
 if (!$userId) {
     $url = '../../Incorrect_request.php';
@@ -49,30 +52,54 @@ foreach ($delete_date as $date) {
 ?>
 
 
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../../../public/img/favicon.ico">
-    <link rel="stylesheet" href="../../../public/css/intern/view.css">
-    <title>「Real intentioN」 / インターン体験記</title>
-    <!-- font-awesomeのインポート -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    <link rel="shortcut icon" href="../../../public/img/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         body {
-            background-color: #e6e6e6;
+            background-color: #eaf0f0;
         }
 
+        header {
+            background-color: #D6E4E5;
+        }
 
+        footer {
+            background-color: #D6E4E5;
+        }
+
+        .nav-link {
+            font-weight: bold;
+        }
+
+        .nav-link:hover {
+            text-decoration: underline;
+        }
+
+        .login-btn {
+            background-color: #EB6440;
+            color: white;
+        }
+
+        .login-btn:hover {
+            color: white;
+            background-color: #eb6540c1;
+        }
 
         .square_box {
             position: relative;
             max-width: 100px;
-            background: #ffb6c1;
+            background: #7F95D1;
+            border-radius: 5px;
         }
 
         .square_box::before {
@@ -86,31 +113,57 @@ foreach ($delete_date as $date) {
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
+            font-weight: bold;
         }
 
-        .side-area {
-            position: sticky;
-            top: 60px;
+        .intern-contents {
+            background-color: white;
+            border-radius: 5px;
+        }
+
+        .area1 p {
+            font-weight: bold;
+        }
+
+        .student-review {
+            color: #FCCA4D;
+        }
+
+        .side-bar {
+            padding-top: 10px;
         }
     </style>
+    <title>「Real IntentioN」 / インターン体験記</title>
 </head>
 
 <body>
-
-    <!-- テスト-------------------------------------------------------------------------------------------- -->
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light py-4">
+        <nav class="navbar navbar-expand-lg navbar-light py-4">
             <div class="container">
-                <img style="width: 45px; height:45px; margin-right:10px;" src="../../../public/img/logo.png" alt="">
-                <a class="navbar-brand" href="#">Real intentioN</a>
+                <a class="navbar-brand" href="./view.php">
+                    <img src="../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
+                                align-text-top" style="object-fit: cover;">
+                    Real intentioN
+                </a>
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span></button>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">職員の方はこちら</a>
+                            <a class="nav-link" href="../../StaffView/login/login_form.php">
+                                <i class="fa-sharp fa-solid fa-user fa-2x"></i><span class="badge fs-5 text text-danger">9</span>
+                            </a>
                         </li>
-                        <button class="btn btn-primary ms-3">ログインはこちら</button>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../StaffView/login/login_form.php">
+                                <i class="fa-sharp fa-solid fa-bell fa-2x"></i><span class="badge fs-5 text text-danger">9</span>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -119,14 +172,18 @@ foreach ($delete_date as $date) {
 
 
 
-    <!-- <div class="bg-light"> -->
-    <main role="main" class="container mt-5" style="padding: 0px">
-        <div class="row">
 
+    <div id="fixed">
+        <a href="./post/post_form.php">インターン体験記<br>を投稿する！</a>
+    </div>
+
+    <!-- <div class="bg-light"> -->
+    <main role="main" class="container mt-5">
+        <div class="row">
             <div class="col-md-8">
                 <?php if (is_array($delete_date) || is_object($delete_date)) : ?>
                     <?php foreach ($delete_date as $row) : ?>
-                        <div class="mb-5 bg-light">
+                        <div class="intern-contents mb-5">
 
                             <!-- area1 -->
                             <div class="area1 d-flex px-3 py-4">
@@ -136,64 +193,199 @@ foreach ($delete_date as $date) {
                                     </div>
                                 </div>
 
-                                <div class="info-center col-10">
-                                    <?php h($row['company']) ?><span style="margin: 0 10px;">/</span><?php h($row['field']) ?><span style="margin: 0 10px;">/</span><?php h($row['format']) ?>
+                                <div class="info-center col-9">
+                                    <p class="fs-5">
+                                        <?php h($row['company']) ?><span style="margin: 0 10px;">/</span><?php h($row['field']) ?><span style="margin: 0 10px;">/</span><?php h($row['format']) ?>
+                                    </p>
 
-                                    <p><?php h($row['content']) ?>s</p>
-
-                                    <p><?php h($row['ster']) ?></p>
+                                    <span><?php h($row['content']) ?></span>
+                                    <br>
+                                    <span class="student-review">
+                                        <?php if ($row['ster'] == 1) : ?>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star"></i>
+                                            <i class="bi bi-star"></i>
+                                            <i class="bi bi-star"></i>
+                                            <i class="bi bi-star"></i>
+                                        <?php elseif ($row['ster'] == 2) : ?>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star"></i>
+                                            <i class="bi bi-star"></i>
+                                            <i class="bi bi-star"></i>
+                                        <?php elseif ($row['ster'] == 3) : ?>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star"></i>
+                                            <i class="bi bi-star"></i>
+                                        <?php elseif ($row['ster'] == 4) : ?>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star"></i>
+                                        <?php elseif ($row['ster'] == 5) : ?>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                        <?php endif; ?>
+                                    </span>
                                 </div>
 
+                                <div class="info-right col-1 ms-4">
 
+                                    <div class="text-center">
+                                        <div class="btn-group">
+                                            <?php if ($userId == $row['user_id']) : ?>
+                                                <div class="btn-group dropstart" role="group">
+                                                    <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    </button>
+                                                    <ul class="dropdown-menu dropdown-menu-dark">
+                                                        <li><a href="./delete/delete_check.php?post_id=<?php h($row['post_id']) ?>" class="dropdown-item">削除</a></li>
+                                                        <li><a class="dropdown-item" href="./update/update_form.php?post_id=<?php h($row['post_id']) ?>">編集</a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+
+
+                                </div>
                             </div>
 
-                            <div class="question px-3">
-                                <span>Q.</span><?php h($row['question']) ?>
+                            <div class="question px-4">
+                                <p style="font-weight: bold;">
+                                    <span style="color: blue;">Q.　</span>
+                                    <?php h($row['question']) ?>
+                                </p>
                             </div>
 
-                            <div class="answer px-3">
-                                <span>A.</span><?php h($row['answer']) ?>
+                            <div class="answer px-4">
+                                <p>
+                                    <span style="color: red; font-weight: bold;">A.　</span>
+                                    <span style="word-break: break-all; white-space: pre-line;"><?php h($row['answer']) ?></span>
+                                </p>
                             </div>
 
                             <div class="area2 d-flex px-3 py-4">
-                                <div class="post-name col-5 pt-2">
-                                    <?php h($row['name']) ?> ｜ <?php h($row['department']) ?> ｜ <?php h($row['school_year']) ?>
-                                </div>
-                            </div>
+                                <div class="question-btn col-5">
+                                    <a href="../view.php" class="btn btn-primary px-4">削除キャンセル</a>
+                                    <a class="login-btn btn px-4" href="./delete.php?post_id=<?php h($delete_post_id) ?>">削除実行</a>
 
-                            <div class="btn-group">
-                                <div>
-                                    <a href="../view.php" class="btn btn-primary">削除キャンセル</a>
                                 </div>
 
-
-                                <a href="./delete.php?post_id=<?php h($delete_post_id) ?>">削除</a>
-                                <!--  -->
-
+                                <div class="post-name col-7 pt-2">
+                                    <p class="text-end">
+                                        <?php h($row['name']) ?> ｜ <?php h($row['department']) ?> ｜ <?php h($row['school_year']) ?>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     <?php endforeach; ?>
                 <?php endif; ?>
+
+                <!-- ページネーション -->
+                <div class="justify-content-center">
+                    <nav aria-label="Page navigation example justify-content-center">
+                        <ul class="pagination">
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
 
+            <div class="side-bar col-md-4 bg-light sticky-top vh-100">
+                <div class="d-flex flex-column flex-shrink-0 p-3 bg-light">
+                    <ul class="nav nav-pills flex-column mb-auto">
+                        <li class="nav-item">
+                            <a style="background-color: #EB6440;" href="./view.php" class="nav-link active" aria-current="page">
+                                インターン体験記
+                            </a>
+                        </li>
+                        <li>
+                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
+                                インターン / イベント情報 / 説明会情報
+                            </a>
+                        </li>
+                    </ul>
 
 
-            <div class="col-md-4 bg-warning sticky-top vh-100">
-                <div>
-                    <h1>送信</h1>
-                    <a href="./post/post_form.php">新規投稿</a>
+
+                    <hr>
+                    <div class="dropdown">
+                        検索BOX
+                    </div>
+
+                    <hr>
+                    <div class="dropdown">
+                        人気の情報を表示
+                    </div>
+
+                    <hr>
+                    <div class="dropdown">
+                        <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                            <strong><?php h($userName) ?></strong>
+                        </a>
+                        <ul class="dropdown-menu text-small shadow">
+                            <li><a class="dropdown-item" href="#">プロフィール</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="../logout.php">サインアウト</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <!-- <ul class=" list-group">
-                    <li class="list-group-item list-group-item-light">Latest Posts</li>
-                    <li class="list-group-item list-group-item-light">Announcements</li>
-                </ul> -->
-            </div><!-- col-md-4 終了-->
-
-
-
-        </div><!-- Div row 終了-->
+            </div>
+        </div>
     </main>
-    <!-- </div> -->
+
+
+
+    <footer>
+        <nav class="navbar navbar-expand-lg navbar-light py-4">
+            <div class="container">
+                <a class="navbar-brand" href="../../../index.html">
+                    <img src="../../../public/img" alt="" width="30" height="24" class="d-inline-block
+                                align-text-top" style="object-fit: cover;">
+                    Real intentioN
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav2">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./src/StaffView/login/login_form.php">職員の方はこちら</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="login-btn btn" href="./src/UserView/login/login_form.php">ログインはこちら</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </footer>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
