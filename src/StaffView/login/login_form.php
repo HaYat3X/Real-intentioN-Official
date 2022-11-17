71833c45b235d07668ea500dd3d49fcf77048c3b
hayategari <hayatesato1@gmail.com>
2022-11-17 21:41:00 +0900

    デザイン修正

--- src/StaffView/login/login_form.php
+++ src/StaffView/login/login_form.php
@@ -2,15 +2,24 @@
 
 session_start();
 
-// functionファイルインポート
-require __DIR__ . '../../../../function/functions.php';
+// 外部ファイルのインポート
+require '../../../class/SystemLogic.php';
+require '../../../function/functions.php';
 
-// // ログインしている場合にはログインフォームを表示させない
-// $result = UserLogic::checkLogin();
-// if ($result) {
-//     header('Location: mypage.php');
-//     return;
-// }
+// インスタンス化
+$val_inst = new DataValidationLogics();
+$arr_prm_inst = new ArrayParamsLogics();
+$db_inst = new DatabaseLogics();
+$staff_inst = new StaffLogics();
+
+// ログインチェック
+$userId = $staff_inst->get_staff_id();
+
+// ログインしている場合はログインフォームを表示させない
+if ($userId) {
+    $url = '../control/post_list.php';
+    header('Location:' . $url);
+}
 
 ?>
 
@@ -21,67 +30,146 @@ require __DIR__ . '../../../../function/functions.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
-    body {
-        background-color: #E6E6E6;
-    }
+        body {
+            background-color: #EFF5F5;
+        }
 
-    form h1 {
-        margin-top: 100px;
-        margin-bottom: 50px;
-    }
+        header {
+            background-color: #D6E4E5;
+        }
+
+        footer {
+            background-color: #497174;
+        }
+
+        .nav-link {
+            font-weight: bold;
+        }
+
+        .nav-link:hover {
+            text-decoration: underline;
+        }
+
+        .login-btn {
+            background-color: #EB6440;
+            color: white;
+        }
+
+        .login-btn:hover {
+            color: white;
+            background-color: #eb6540c1;
+        }
+
+        .box {
+            background-color: white;
+            border-radius: 5px;
+        }
     </style>
     <title>Document</title>
 </head>
 
 <body>
+
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light py-4">
+        <nav class="navbar navbar-expand-lg navbar-light py-4">
             <div class="container">
-                <a class="navbar-brand" href="#">Real intentioN</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span></button>
+                <a class="navbar-brand" href="../../../index.html">
+                    <img src="../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
+                                align-text-top" style="object-fit: cover;">
+                    Real intentioN
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./src/StaffView/login/login_form.php">Real intentioNとは</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="./src/StaffView/login/login_form.php">お問い合わせはこちら</a>
+                        </li>
+                    </ul>
+                </div>
+
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">職員の方はこちら</a>
+                            <a class="nav-link" href="../../StaffView/login/login_form.php">職員の方はこちら</a>
                         </li>
-                        <button class="btn btn-primary ms-3">ログインはこちら</button>
+
+                        <li class="nav-item">
+                            <a class="login-btn btn" href="./login_form.php">ログインはこちら</a>
+                        </li>
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
 
-    <div class="main d-flex align-items-center">
-        <div class="container">
-            <div class="row">
-                <div class="mx-auto col-lg-6">
-                    <form class="mt-5" action="./login.php" method="post">
-                        <h1 class="text-center">ログイン</h1>
 
-                        <div class="mb-2">
-                            <label class="form-label" for="name">メールアドレス</label>
-                            <input class="form-control" type="text" name="email" id="name">
-                        </div>
+    
 
-                        <div class="mb-2">
-                            <label class="form-label" for="name">パスワード</label>
-                            <input class="form-control" type="password" name="password" id="name">
-                        </div>
-                        <button type="submit" class="btn btn-primary px-5">ログインする</button>
-                        <a href="../register/staff_auth_form.php">未登録の先生はこちら</a>
-                    </form>
-                </div>
+    <div class="container">
+        <div class="box my-5 py-5">
+            <div class="mx-auto col-lg-5">
+                <form action="./login.php" method="post">
+                    <h1 class="text-center fs-1 mb-5">ログインする（職員）</h1>
+
+                    <div class="mb-4">
+                        <label class="form-label" for="name">メールアドレス</label>
+                        <input class="form-control" type="text" name="email" id="name">
+                    </div>
+
+                    <div class="mb-4">
+                        <label class="form-label" for="name">パスワード</label>
+                        <input class="form-control" type="password" name="password" id="name">
+                    </div>
+                    <button type="submit" class="login-btn btn px-4">ログインする</button>
+                    <a class="btn btn-primary" href="../register/staff_auth_form.php">未登録の先生はこちら</a>
+                </form>
             </div>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+
+
+
+    <footer>
+        <nav class="navbar navbar-expand-lg navbar-light py-4">
+            <div class="container">
+                <a class="navbar-brand" href="../../../index.html">
+                    <img src="../../../public/img" alt="" width="30" height="24" class="d-inline-block
+                                align-text-top" style="object-fit: cover;">
+                    Real intentioN
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav2">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./src/StaffView/login/login_form.php">職員の方はこちら</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="login-btn btn" href="./src/UserView/login/login_form.php">ログインはこちら</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 </body>
 
