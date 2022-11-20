c4d647309489de68779dedb00ce7b036c718faab
hayategari <hayatesato1@gmail.com>
2022-11-20 11:12:33 +0900

    デザイン修正

--- src/UserView/Intern_experience/delete/delete_check.php
+++ src/UserView/Intern_experience/delete/delete_check.php
@@ -74,7 +74,7 @@ foreach ($delete_date as $date) {
         }
 
         footer {
-            background-color: #D6E4E5;
+            background-color: #497174;
         }
 
         .nav-link {
@@ -121,16 +121,9 @@ foreach ($delete_date as $date) {
             border-radius: 5px;
         }
 
-        .area1 p {
-            font-weight: bold;
-        }
-
-        .student-review {
-            color: #FCCA4D;
-        }
-
         .side-bar {
             padding-top: 10px;
+            padding-bottom: 10px;
         }
     </style>
     <title>「Real IntentioN」 / インターン体験記</title>
@@ -141,7 +134,7 @@ foreach ($delete_date as $date) {
         <nav class="navbar navbar-expand-lg navbar-light py-4">
             <div class="container">
                 <a class="navbar-brand" href="./view.php">
-                    <img src="../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
+                    <img src="../../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
                                 align-text-top" style="object-fit: cover;">
                     Real intentioN
                 </a>
@@ -149,23 +142,6 @@ foreach ($delete_date as $date) {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../../StaffView/login/login_form.php">
-                                <i class="fa-sharp fa-solid fa-user fa-2x"></i><span class="badge fs-5 text text-danger">9</span>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="../../StaffView/login/login_form.php">
-                                <i class="fa-sharp fa-solid fa-bell fa-2x"></i><span class="badge fs-5 text text-danger">9</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
     </header>
@@ -286,29 +262,24 @@ foreach ($delete_date as $date) {
             <div class="side-bar col-md-4 bg-light sticky-top h-100">
                 <div class="d-flex flex-column flex-shrink-0 p-3 bg-light">
                     <ul class="nav nav-pills flex-column mb-auto">
-                        <li class="nav-item">
-                            <a style="background-color: #EB6440;" href="./view.php" class="nav-link active" aria-current="page">
+                        <li>
+                            <a href="../../staff_information/staff_information.php" class="nav-link link-dark">
+                                インターン情報　/ 説明会情報
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="../view.php" class="nav-link link-dark" aria-current="page">
                                 インターン体験記
                             </a>
                         </li>
+
                         <li>
-                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
-                                インターン / イベント情報 / 説明会情報
+                            <a href="./post_form.php" class="nav-link link-dark">
+                                インターン体験記を新規投稿
                             </a>
                         </li>
                     </ul>
-
-
-
-                    <hr>
-                    <div class="dropdown">
-                        検索BOX
-                    </div>
-
-                    <hr>
-                    <div class="dropdown">
-                        人気の情報を表示
-                    </div>
 
                     <hr>
                     <div class="dropdown">
@@ -317,11 +288,19 @@ foreach ($delete_date as $date) {
                             <strong><?php h($userName) ?></strong>
                         </a>
                         <ul class="dropdown-menu text-small shadow">
-                            <li><a class="dropdown-item" href="#">プロフィール</a></li>
-                            <li>
+                            <!-- <li>
+                                <a class="dropdown-item" href="#">プロフィール
+
+                                </a>
+                            </li> -->
+
+                            <!-- <li>
                                 <hr class="dropdown-divider">
+                            </li> -->
+
+                            <li>
+                                <a class="dropdown-item" href="../../logout.php">サインアウト</a>
                             </li>
-                            <li><a class="dropdown-item" href="../logout.php">サインアウト</a></li>
                         </ul>
                     </div>
                 </div>
@@ -334,11 +313,13 @@ foreach ($delete_date as $date) {
     <footer>
         <nav class="navbar navbar-expand-lg navbar-light py-4">
             <div class="container">
-                <a class="navbar-brand" href="../../../index.html">
-                    <img src="../../../public/img" alt="" width="30" height="24" class="d-inline-block
-                                align-text-top" style="object-fit: cover;">
-                    Real intentioN
-                </a>
+                <div class="col-md-4 d-flex align-items-center">
+                    <a href="../../../../index.html" class="mb-3 me-2 mb-md-0
+                                text-muted text-decoration-none lh-1"><img src="../../../../public/img/logo.png" width="30px" height="30px" alt=""></a>
+                    <span class="mb-3 mb-md-0" style="color: rgba(255,
+                                255, 255, 0.697);">&copy;
+                        2022 Toge-Company, Inc</span>
+                </div>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -347,17 +328,28 @@ foreach ($delete_date as $date) {
                 <div class="collapse navbar-collapse" id="navbarNav2">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="./src/StaffView/login/login_form.php">職員の方はこちら</a>
+                            <a class="nav-link" target="_blank" href="https://github.com/Hayate12345">
+                                <img src="../../../../public/img/icons8-github-120.png" width="35px" height="35px" alt="">
+                            </a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="login-btn btn" href="./src/UserView/login/login_form.php">ログインはこちら</a>
+                            <a class="nav-link" target="_blank" href="https://hayate-takeda.xyz/">
+                                <img src="../../../../public/img/icons8-ポートフォリオ-100.png" width="30px" height="30px" alt="">
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" target="_blank" href="https://twitter.com/hayate_KIC">
+                                <img src="../../../../public/img/icons8-ツイッター-100.png" width="30px" height="30px" alt="">
+                            </a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
     </footer>
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
