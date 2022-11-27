90cca6652255a6ed4553ffce241ef7dbc9ff1c11
hayategari <hayatesato1@gmail.com>
2022-11-27 09:53:33 +0900

    最終開発の開始

---
+++ src/student_view/register/provisional_registration.php
@@ -0,0 +1,27 @@
+<?php
+
+// フォームリクエストがあるかないか判定する
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+}else{
+    
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    TestCase
+</body>
+
+</html>
---
+++ src/student_view/register/provisional_registration_form.php
@@ -0,0 +1,24 @@
+<?php
+
+// csrf対策
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="./provisional_registration.php" method="post">
+        <input type="text" name="email">
+        <button type="submit">仮登録する</button>
+    </form>
+</body>
+
+</html>
