f15ff7f1f65bb37b627452f01afa5b26a9467199
hayategari <hayatesato1@gmail.com>
2022-12-03 14:54:05 +0900

    てxt

---
+++ src/template/footer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../../../public/img/favicon.ico" type="image/x-icon">
+    <title>学生情報登録 / 「Real intentioN」</title>
+    <style>
+        body {
+            background-color: #EFF5F5;
+        }
+
+        header {
+            background-color: #D6E4E5;
+        }
+
+        footer {
+            background-color: #497174;
+        }
+
+        .nav-link:hover {
+            text-decoration: underline;
+        }
+
+        .login-btn {
+            background-color: #EB6440;
+            color: white;
+        }
+
+        .login-btn:hover {
+            color: white;
+            background-color: #eb6540c4;
+        }
+    </style>
+</head>
+
+<body>
+    <footer class="text-center py-2">
+        <div class="text-light text-center small">
+            © 2020 Toge-company:
+            <a class="text-white" target="_blank" href="https://hayate-takeda.xyz/">hayate-takeda.xyz</a>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- src/view/student_view/provisional_registration_form.php
+++ src/view/student_view/provisional_registration_form.php
@@ -57,32 +57,9 @@ $rgs_calc = new Register();
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light py-4">
-            <div class="container">
-                <a class="navbar-brand" href="./index.html">
-                    <img src="../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
-                                align-text-top" style="object-fit: cover;"> Real intentioN
-                </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link fw-bold" href="./src/StaffView/login/login_form.php">職員の方はこちら</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="login-btn btn px-3" href="./src/UserView/login/login_form.php">ログインはこちら</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <!-- ヘッダーテンプレート -->
+    <?php include(PATH . '/src/template/header.php'); ?>
 
     <div class="box d-flex vh-100 align-items-center">
         <div class="container bg-light py-5">
@@ -113,13 +90,7 @@ $rgs_calc = new Register();
         </div>
     </div>
 
-
-    <footer class="text-center py-2">
-        <div class="text-light text-center small">
-            © 2020 Toge-company:
-            <a class="text-white" target="_blank" href="https://hayate-takeda.xyz/">hayate-takeda.xyz</a>
-        </div>
-    </footer>
+    <?php include(PATH . '/src/template/footer.php') ?>
 
     <script>
         (() => {
