8396f8a2c74110b885ff8b5520df8991ae2d9ede
hayategari <hayatesato1@gmail.com>
2022-12-10 22:22:20 +0900

    インターン体験記編集機能完成

---
+++ class/Update_calc.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once '/Applications/MAMP/htdocs/Deliverables4/class/Database_calc.php';
+
+class Update
+{
+    /**
+     * インターン体験記を編集する
+     */
+    public function intern_experience_update($user_id, $company, $format, $content, $question, $answer, $ster, $field, $post_id)
+    {
+        $db_calc = new Database();
+
+        $sql = "UPDATE `intern_experience_tbl` SET `user_id` = ?, `company` = ?, `format` = ?, `content` = ?, `question` = ?, `answer` = ?, `ster` = ?, `field` = ? WHERE post_id = ?";
+
+        $argument = [];
+        $argument[] = strval($user_id);
+        $argument[] = strval($company);
+        $argument[] = strval($format);
+        $argument[] = strval($content);
+        $argument[] = strval($question);
+        $argument[] = strval($answer);
+        $argument[] = strval($ster);
+        $argument[] = strval($field);
+        $argument[] = strval($post_id);
+
+        $result = $db_calc->data_various_kinds($sql, $argument);
+
+        return $result;
+    }
+}
--- class/View_calc.php
+++ class/View_calc.php
@@ -17,4 +17,21 @@ class View
 
         return $result;
     }
+
+    /**
+     * インターン体験記の投稿一件を取得
+     */
+    public function intern_experience_data_one($post_id)
+    {
+        $db_calc = new Database();
+
+        $sql = 'SELECT * FROM `intern_experience_tbl` INNER JOIN `student_mst` ON intern_experience_tbl.user_id = student_mst.student_id AND intern_experience_tbl.post_id = ?';
+
+        $argument = [];
+        $argument[] = $post_id;
+
+        $result = $db_calc->data_select_argument($sql, $argument);
+
+        return $result;
+    }
 }
