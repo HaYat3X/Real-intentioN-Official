8287d9a23b89e409f6e35f2cb7111de16d15585a
hayategari <hayatesato1@gmail.com>
2022-12-18 19:09:37 +0900

    リファクタリング

--- src/Exception/400_request.php
+++ src/Exception/400_request.php
@@ -18,19 +18,37 @@ header('refresh:3;url=' . $uri);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="shortcut icon" href="../../../../public/img/favicon.ico" type="image/x-icon">
-    <title>サービス利用登録 / 「Real intentioN」</title>
+    <link rel="shortcut icon" href="../../public/img/favicon.ico" type="image/x-icon">
+    <title>不正なリクエスト /「Real intentioN」</title>
     <style>
         body {
             background-color: #EFF5F5;
         }
 
         header {
-            background-color: #D6E4E5;
+            background-color: #c2dbde;
         }
 
         footer {
             background-color: #497174;
+            margin-top: 120px;
+        }
+
+        .footer-top {
+            padding-bottom: 90px;
+            padding: 90px;
+        }
+
+        .footer-top a {
+            color: #fff;
+        }
+
+        .footer-top a:hover {
+            color: #fff;
+        }
+
+        .nav-link {
+            font-weight: bold;
         }
 
         .nav-link:hover {
@@ -46,6 +64,96 @@ header('refresh:3;url=' . $uri);
             color: white;
             background-color: #eb6540c4;
         }
+
+        section {
+            padding-top: 120px;
+        }
+
+        .hero {
+            background-image: url('./public/img/92d501bc70777a3bf854e9e1aab4881d.jpg');
+            background-position: center;
+            background-size: cover;
+            background-attachment: fixed;
+            position: relative;
+            z-index: 2;
+        }
+
+        .hero::after {
+            content: "";
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            background-color: rgba(37, 39, 71, 0.3);
+            z-index: -1;
+        }
+
+        .card-effect {
+            box-shadow: blue;
+            background-color: #fff;
+            padding: 25px;
+            transition: all 0.35s ease;
+        }
+
+        .card-effect:hover {
+            box-shadow: none;
+            transform: translateY(5px);
+        }
+
+        .iconbox {
+            width: 54px;
+            height: 54px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #EB6440;
+            color: white;
+            font-size: 32px;
+            border-radius: 100px;
+            flex: none;
+        }
+
+        .service {
+            position: relative;
+            z-index: 2;
+            overflow: hidden;
+        }
+
+        .service::after {
+            content: "";
+            position: absolute;
+            top: -100%;
+            left: 0;
+            background-color: #EB6440;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+            opacity: 0;
+            transition: all 0.4s ease;
+        }
+
+        .service:hover h5,
+        .service:hover p {
+            color: white;
+        }
+
+        .service:hover .iconbox {
+            background-color: #fff;
+            color: #EB6440;
+        }
+
+        .service:hover::after {
+            opacity: 1;
+            top: 0;
+        }
+
+        .col-img {
+            background-image: url('./public/img/kaihatusya.png');
+            background-position: center;
+            background-size: cover;
+            min-height: 480px;
+        }
     </style>
 </head>
 
@@ -53,8 +161,8 @@ header('refresh:3;url=' . $uri);
     <header class="sticky-top">
         <nav class="navbar navbar-expand-lg navbar-light py-4">
             <div class="container">
-                <a class="navbar-brand" href="./index.html">
-                    <img src="../../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
+                <a class="navbar-brand" href="../../index.html">
+                    <img src="../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
                             align-text-top" style="object-fit: cover;"> Real intentioN
                 </a>
 
@@ -65,10 +173,10 @@ header('refresh:3;url=' . $uri);
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link px-4" href="./src/StaffView/login/login_form.php">職員の方はこちら</a>
+                            <a class="nav-link px-4" href="../view/staff_view/login/login_form.php">職員の方はこちら</a>
                         </li>
                         <li class="nav-item">
-                            <a class="login-btn btn px-4" href="./src/UserView/login/login_form.php">ログインはこちら</a>
+                            <a class="login-btn btn px-4" href="../view/student_view/login/login_form.php">ログインはこちら</a>
                         </li>
                     </ul>
                 </div>
