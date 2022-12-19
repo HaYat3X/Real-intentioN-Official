d65759ed5ae867d61a6e56bf75562cdd2fcccbbb
hayategari <hayatesato1@gmail.com>
2022-12-20 04:29:54 +0900

    リファクタリング

--- src/view/student_view/login/login_form.php
+++ src/view/student_view/login/login_form.php
@@ -21,7 +21,7 @@ $ses_calc = new Session();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     <link href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css" rel="stylesheet" />
-    <link rel="shortcut icon" href="../../../public/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../../../../public/img/favicon.ico" type="image/x-icon">
     <title>学生ログイン /「Real intentioN」</title>
     <style>
         body {
@@ -190,7 +190,7 @@ $ses_calc = new Session();
     <div class="box d-flex vh-100 align-items-center">
         <div class="container bg-light py-5">
             <div class="row py-5">
-                <div class="col-lg-5 mx-auto">
+                <div class="col-lg-5 col-md-11 col-11 mx-auto">
                     <form class="needs-validation" novalidate action="./login.php" method="POST">
                         <h1 class="text-center fs-2 mb-5">
                             ログインする（学生）
