c57d5e0ae1e2b0dcf78746170a8d6bf9031e90d3
hayategari <hayatesato1@gmail.com>
2022-12-27 02:03:07 +0900

    リファクタリング

--- src/view/student_view/intern_experience/update/update_form.php
+++ src/view/student_view/intern_experience/update/update_form.php
@@ -31,9 +31,14 @@ foreach ($student_login_data as $row) {
     $user_name = $row['name'];
 }
 
+// ユーザアイコンを抽出
+foreach ($student_login_data as $row) {
+    $user_icon = $row['icon'];
+}
+
 // ログイン情報がない場合リダイレクト
 if (!$student_login_data) {
-    $uri = '../../../Exception/400_request.php';
+    $uri = '../../../../Exception/400_request.php';
     header('Location: ' . $uri);
 }
 
@@ -45,7 +50,7 @@ $update_data = $viw_calc->intern_experience_data_one($post_id);
 
 // 編集するデータがない場合はリダイレクト
 if (!$update_data) {
-    $uri = '../../../Exception/400_request.php';
+    $uri = '../../../../Exception/400_request.php';
     header('Location: ' . $uri);
 }
 
@@ -56,7 +61,7 @@ foreach ($update_data as $row) {
 
 // 編集権限がない場合はリダイレクト
 if ($post_user_id !== $user_id) {
-    $uri = '../../../Exception/400_request.php';
+    $uri = '../../../../Exception/400_request.php';
     header('Location: ' . $uri);
 }
 
@@ -79,7 +84,7 @@ if ($post_user_id !== $user_id) {
         }
 
         header {
-            background-color: #D6E4E5;
+            background-color: #c2dbde;
         }
 
         footer {
@@ -157,7 +162,7 @@ if ($post_user_id !== $user_id) {
                         <div class="bg-light py-5">
                             <form class="needs-validation col-lg-7 col-md-9 col-11 mx-auto" novalidate action="./update.php" method="POST">
                                 <h1 class="text-center fs-2 mb-5">
-                                    インターン体験記を編集する
+                                    インターンシップ体験記を編集
                                 </h1>
 
                                 <div class="mt-4">
@@ -254,7 +259,7 @@ if ($post_user_id !== $user_id) {
                                 <input type="hidden" name="csrf_token" value="<?php h($ses_calc->create_csrf_token()); ?>">
 
                                 <div class="mt-4">
-                                    <button class="btn login-btn px-4" onclick="alertFunction1()">投稿する</button>
+                                    <button class="btn login-btn px-4" onclick="alertFunction1()">更新する</button>
                                 </div>
                             </form>
                         </div>
@@ -266,19 +271,19 @@ if ($post_user_id !== $user_id) {
                 <div class="d-flex flex-column flex-shrink-0 p-3 bg-light">
                     <ul class="nav nav-pills flex-column mb-auto">
                         <li class="nav-item">
-                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
+                            <a href="../../intern_information/posts_recommendation.php" class="nav-link link-dark">
                                 インターンシップ情報
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
+                            <a href="../../briefing_information/posts_recommendation.php" class="nav-link link-dark">
                                 会社説明会情報
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
+                            <a href="../../kic_notification/posts.php" class="nav-link link-dark">
                                 キャリアセンターからのお知らせ
                             </a>
                         </li>
@@ -290,19 +295,19 @@ if ($post_user_id !== $user_id) {
                         </li>
 
                         <li>
-                            <a href="./post/post_form.php" class="nav-link link-dark">
+                            <a href="../../es_experience/posts.php" class="nav-link link-dark">
                                 ES体験記
                             </a>
                         </li>
 
                         <li>
-                            <a href="./post/post_form.php" class="nav-link link-dark">
+                            <a href="../post/post_form.php" class="nav-link link-dark">
                                 インターンシップ体験記を投稿
                             </a>
                         </li>
 
                         <li>
-                            <a href="./post/post_form.php" class="nav-link link-dark">
+                            <a href="../../es_experience/post/post_form.php" class="nav-link link-dark">
                                 ES体験記を投稿
                             </a>
                         </li>
@@ -311,75 +316,20 @@ if ($post_user_id !== $user_id) {
                     <hr>
 
                     <div class="dropdown">
-                        <div class="mb-4">
-                            <form action="../search/search_result.php" method="post">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" name="keyword" placeholder="フリーワード検索">
-                                    <input type="hidden" name="category" value="answer">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="bi bi-search"></i></button>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="mb-4">
-                            <form action="../search/search_result.php" method="post">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" name="keyword" placeholder="企業名で検索">
-                                    <input type="hidden" name="category" value="company">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="bi bi-search"></i></button>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="mb-4">
-                            <form action="../search/search_result.php" method="post">
-                                <div class="input-group">
-                                    <select class="form-select" name="keyword" aria-label="Default select example">
-                                        <option selected>開催形式で検索</option>
-                                        <option value="対面開催">対面開催</option>
-                                        <option value="オンライン開催">オンライン開催</option>
-                                    </select>
-                                    <input type="hidden" name="category" value="format">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="bi bi-search"></i></button>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="mb-4">
-                            <form action="../search/search_result.php" method="post">
-                                <div class="input-group">
-                                    <select class="form-select" name="keyword" aria-label="Default select example">
-                                        <option selected>職種分野で検索</option>
-                                        <option value="IT分野">IT分野</option>
-                                        <option value="ゲームソフト分野">ゲームソフト分野</option>
-                                        <option value="ハード分野">ハード分野</option>
-                                        <option value="ビジネス分野">ビジネス分野</option>
-                                        <option value="CAD分野">CAD分野</option>
-                                        <option value="グラフィックス分野">グラフィックス分野</option>
-                                        <option value="サウンド分野">サウンド分野</option>
-                                        <option value="日本語分野">日本語分野</option>
-                                        <option value="国際コミュニケーション分野">国際コミュニケーション分野</option>
-                                    </select>
-                                    <input type="hidden" name="category" value="field">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="bi bi-search"></i></button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-                    <hr>
-
-                    <div class="dropdown">
                         <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                            <?php if ($user_icon === "") : ?>
+                                <img src="../../../../../public/ICON/default-icon.jpeg" width="32" height="32" class="rounded-circle me-2" style="object-fit: cover;">
+                            <?php else : ?>
+                                <img src="../../../../../public/ICON/<?php h($user_icon) ?>" width="32" height="32" class="rounded-circle me-2" style="object-fit: cover;">
+                            <?php endif; ?>
                             <strong><?php h($user_name) ?></strong>
                         </a>
                         <ul class="dropdown-menu text-small shadow">
-                            <li><a class="dropdown-item" href="#">プロフィール</a></li>
+                            <li><a class="dropdown-item" href="../../profile/profile.php">プロフィール</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="../logout.php">サインアウト</a></li>
+                            <li><a class="dropdown-item" href="../../../../logout/logout.php">ログアウト</a></li>
                         </ul>
                     </div>
                 </div>
