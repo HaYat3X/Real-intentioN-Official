f4bf936a2f17acfcbf4a32c81a5962e5551596bf
hayategari <hayatesato1@gmail.com>
2022-12-31 04:21:53 +0900

    リファクタリング

--- src/view/staff_view/briefing_information/reserve/reserve_list.php
+++ src/view/staff_view/briefing_information/reserve/reserve_list.php
@@ -33,10 +33,11 @@ foreach ($staff_login_data as $row) {
 
 // ログイン情報がない場合リダイレクト
 if (!$staff_login_data) {
-    $uri = '../../../Exception/400_request.php';
+    $uri = '../../../../Exception/400_request.php';
     header('Location: ' . $uri);
 }
 
+// パラメータから投稿IDを取得
 $post_id = filter_input(INPUT_GET, 'post_id');
 
 // インターンシップ情報投稿に予約をした学生情報を取得する
@@ -54,14 +55,14 @@ $reserve_data = $rsv_calc->briefing_information_reserve_data($post_id);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <link rel="shortcut icon" href="../../../../public/img/favicon.ico" type="image/x-icon">
-    <title>ES体験記 /「Real intentioN」</title>
+    <title>会社説明会予約者一覧 /「Real intentioN」</title>
     <style>
         body {
             background-color: #EFF5F5;
         }
 
         header {
-            background-color: #D6E4E5;
+            background-color: #c2dbde;
         }
 
         footer {
@@ -114,138 +115,104 @@ $reserve_data = $rsv_calc->briefing_information_reserve_data($post_id);
         <nav class="navbar navbar-expand-lg navbar-light py-4">
             <div class="container">
                 <a class="navbar-brand" href="">
-                    <img src="../../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
+                    <img src="../../../../../public/img/logo.png" alt="" width="30" height="24" class="d-inline-block
                             align-text-top" style="object-fit: cover;"> Real intentioN
                 </a>
             </div>
         </nav>
     </header>
 
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-lg-8 col-md-12 col-12">
-                <div class="intern-contents mb-5 px-4 py-4 bg-light">
-                    <h1 class="fs-4 mb-4">予約者リスト</h1>
-                    <?php if (is_array($reserve_data) || is_object($reserve_data)) : ?>
-                        <?php foreach ($reserve_data as $row) : ?>
-                            <ul class="list-group">
-                                <li class="list-group-item mb-2"><?php h($row['name']) ?> ｜ <?php h($row['email']) ?> ｜ <?php h($row['course_of_study']) ?> ｜ <?php h($row['grade_in_school']) ?> ｜ <?php h($row['attendance_record_number']) ?></li>
-                            </ul>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                </div>
-            </div>
+    <div class="box d-flex vh-100 align-items-center">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-lg-8 col-md-12 col-12">
+                    <div class="row mb-5 px-4 py-4 bg-light">
+                        <h1 class="fs-4 mt-2 mb-4">予約者一覧</h1>
+                        <table class="table table-striped mt-2">
+                            <thead>
+                                <tr>
+                                    <th scope="col">予約者名</th>
+                                    <th scope="col">メールアドレス</th>
+                                    <th scope="col">所属学科</th>
+                                    <th scope="col">学年</th>
+                                    <th scope="col">出席番号</th>
+                                </tr>
+                            </thead>
 
-            <div class="side-bar col-md-4 bg-light  h-100">
-                <div class="d-flex flex-column flex-shrink-0 p-3 bg-light">
-                    <ul class="nav nav-pills flex-column mb-auto">
-                        <li class="nav-item">
-                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
-                                インターンシップ情報
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
-                                会社説明会情報
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="../staff_information/staff_information.php" class="nav-link link-dark">
-                                キャリアセンターからのお知らせ
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="../intern_experience/posts.php" class="nav-link link-dark">
-                                インターンシップ体験記
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="./posts.php" style="background-color: #EB6440;" class="nav-link active" aria-current="page">
-                                ES体験記
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="./post/post_form.php" class="nav-link link-dark">
-                                インターンシップ体験記を投稿
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="./post/post_form.php" class="nav-link link-dark">
-                                ES体験記を投稿
-                            </a>
-                        </li>
-                    </ul>
-
-                    <hr>
-
-                    <div class="dropdown">
-                        <div class="mb-4">
-                            <form action="./search/search_result.php" method="post">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" name="keyword" placeholder="フリーワード検索">
-                                    <input type="hidden" name="category" value="answer">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="bi bi-search"></i></button>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="mb-4">
-                            <form action="./search/search_result.php" method="post">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" name="keyword" placeholder="企業名で検索">
-                                    <input type="hidden" name="category" value="company">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="bi bi-search"></i></button>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="mb-4">
-                            <form action="./search/search_result.php" method="post">
-                                <div class="input-group">
-                                    <select class="form-select" name="keyword" aria-label="Default select example">
-                                        <option selected>職種分野で検索</option>
-                                        <option value="IT分野">IT分野</option>
-                                        <option value="ゲームソフト分野">ゲームソフト分野</option>
-                                        <option value="ハード分野">ハード分野</option>
-                                        <option value="ビジネス分野">ビジネス分野</option>
-                                        <option value="CAD分野">CAD分野</option>
-                                        <option value="グラフィックス分野">グラフィックス分野</option>
-                                        <option value="サウンド分野">サウンド分野</option>
-                                        <option value="日本語分野">日本語分野</option>
-                                        <option value="国際コミュニケーション分野">国際コミュニケーション分野</option>
-                                    </select>
-                                    <input type="hidden" name="category" value="field">
-                                    <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="bi bi-search"></i></button>
-                                </div>
-                            </form>
-                        </div>
+                            <tbody>
+                                <?php if (is_array($reserve_data) || is_object($reserve_data)) : ?>
+                                    <?php foreach ($reserve_data as $row) : ?>
+                                        <tr>
+                                            <td><?php h($row['name']) ?></td>
+                                            <td><?php h($row['email']) ?></td>
+                                            <td><?php h($row['course_of_study']) ?></td>
+                                            <td><?php h($row['grade_in_school']) ?></td>
+                                            <td><?php h($row['attendance_record_number']) ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
                     </div>
+                </div>
 
-                    <hr>
-
-                    <div class="dropdown">
-                        <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                            <strong><?php h($user_name) ?></strong>
-                        </a>
-                        <ul class="dropdown-menu text-small shadow">
-                            <li><a class="dropdown-item" href="#">プロフィール</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
+                <div class="side-bar col-md-4 bg-light h-100">
+                    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light">
+                        <ul class="nav nav-pills flex-column mb-auto">
+                            <li class="nav-item">
+                                <a href="../../intern_information/posts.php" class="nav-link link-dark">
+                                    インターンシップ情報
+                                </a>
                             </li>
-                            <li><a class="dropdown-item" href="../logout.php">サインアウト</a></li>
+
+                            <li class="nav-item">
+                                <a href="../posts.php" class="nav-link link-dark">
+                                    会社説明会情報
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="../../kic_information/posts.php" class="nav-link link-dark">
+                                    キャリアセンターからのお知らせ
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="../../intern_information/post/post_form.php" class="nav-link link-dark">
+                                    インターンシップ情報を投稿
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../post/post_form.php" class="nav-link link-dark">
+                                    会社説明会情報を投稿
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="../../kic_information/post/post_form.php" class="nav-link link-dark">
+                                    キャリアセンターからのお知らせを投稿
+                                </a>
+                            </li>
                         </ul>
+
+                        <hr>
+
+                        <div class="dropdown">
+                            <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="../../../../../public/ICON/default-icon.jpeg" alt="" width="32" height="32" class="rounded-circle me-2" style="object-fit: cover;">
+                                <strong><?php h($user_name) ?></strong>
+                            </a>
+                            <ul class="dropdown-menu text-small shadow">
+                                <li><a class="dropdown-item" href="../../../../logout/logout.php">ログアウト</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 
     <footer class="text-center py-3">
         <div class="text-light text-center small">
