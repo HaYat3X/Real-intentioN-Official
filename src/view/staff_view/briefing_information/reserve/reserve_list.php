e483adf0f9f8d5e1317340c4e07a8a5ecc58227b
hayategari <hayatesato1@gmail.com>
2022-12-31 04:23:14 +0900

    リファクタリング

--- src/view/staff_view/briefing_information/reserve/reserve_list.php
+++ src/view/staff_view/briefing_information/reserve/reserve_list.php
@@ -54,7 +54,7 @@ $reserve_data = $rsv_calc->briefing_information_reserve_data($post_id);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
-    <link rel="shortcut icon" href="../../../../public/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../../../../../public/img/favicon.ico" type="image/x-icon">
     <title>会社説明会予約者一覧 /「Real intentioN」</title>
     <style>
         body {
